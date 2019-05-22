7c9c8051d5611af39717a509727a0ae54e3847b5
Steve Kroll <steve.m.kroll@gmail.com>
2019-05-22 15:19:38 -0500

    updated form

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,30 +1,26 @@
 <template>
   <form>
-    <fieldset>
-      <input-user :brand="brand" :hasErrors="errors.first('username') ? true : false">
-        <input
-          type="text"
-          name="username"
-          v-model="username"
-          autocomplete="current-password"
-          v-validate.initial="'required|email'"
-        >
-      </input-user>
-      <input-password :brand="brand" :hasErrors="errors.first('password') ? true : false">
-        <input
-          type="password"
-          name="password"
-          v-model="password"
-          autocomplete="current-password"
-          v-validate.initial="'required|min:6'"
-        >
-      </input-password>
-      <btn-success
-        :brand="brand"
-        :disabled="errors.any()"
-        :action="() => showAlert('success')"
-      >login</btn-success>
-    </fieldset>
+    <!-- <fieldset> -->
+    <input-user :brand="brand" :hasErrors="errors.first('username') ? true : false">
+      <input
+        type="text"
+        name="username"
+        v-model="username"
+        autocomplete="current-password"
+        v-validate.initial="'required|email'"
+      >
+    </input-user>
+    <input-password :brand="brand" :hasErrors="errors.first('password') ? true : false">
+      <input
+        type="password"
+        name="password"
+        v-model="password"
+        autocomplete="current-password"
+        v-validate.initial="'required|min:6'"
+      >
+    </input-password>
+    <btn-success :brand="brand" :disabled="errors.any()" :action="() => showAlert('success')">login</btn-success>
+    <!-- </fieldset> -->
   </form>
 </template>
 
@@ -41,7 +37,15 @@ export default {
   methods: {
     showAlert(type) {
       alert("clicked " + type);
+      (this.username = ""), (this.password = "");
     }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+form {
+  padding: 30vw;
+}
+</style>
+
